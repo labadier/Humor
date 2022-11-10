418d4282cab3a3d3cd0ca3d87a3faf7361787dcf
labadier <rlabadiet@gmail.com>
2022-11-10 18:26:32 +0100

    fixed data

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
       mergeData('test')
       print(f"{bcolors.OKCYAN}{bcolors.BOLD}Dataset Made!!{bcolors.ENDC}")
     else:
-      #TranslatePivotLang(tf, output)
+      TranslatePivotLang(tf, output)
       backTranslation(tf)
 
       print(f"{bcolors.OKCYAN}{bcolors.BOLD}Data Translated!!{bcolors.ENDC}")
