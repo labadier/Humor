f3d3db33ed8cda351f29ef78afcae7a19877e915
labadier <rlabadiet@gmail.com>
2022-11-27 11:00:58 +0100

    fix column name

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv
 python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l en -bs 64 -epoches 8 -df data/test.csv -id headlines
 
 # #monolingual - es
-python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l es -bs 64 -epoches 8 -df data/test.csv
+# python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l es -bs 64 -epoches 8 -df data/test.csv
 
 # #multilingual
 # python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l ml -bs 64 -epoches 8 -df data/test.csv
