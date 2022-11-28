686fd467300d17cddbb1fe1c83b5d252688bd7e9
labadier <rlabadiet@gmail.com>
2022-11-28 17:55:43 +0100

    editing

--- exp.py
+++ exp.py
@@ -1,8 +1,16 @@
 import os
+from utils.params import params
 
 for i in range(3):
   for j in ['Haha', 'HaHackathon', 'joker', 'headlines']:
     os.system(f'\
-    python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l ml -bs 64 -epoches 8 -df data/test.csv -mi {i}\
-    -id {j} -desc {j}\
+    python main.py -model encoder -mode predict -wm online -bs 64 -df data/test.csv -output preds \
+    -id {j} -desc {j} -wp logs/{params.model_mult[i]}/{params.model_mult[i]}_{j}.pt\
       ')
+      
+# for i in range(3):
+#   for j in ['Haha', 'HaHackathon', 'joker', 'headlines']:
+#     os.system(f'\
+#     python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l ml -bs 64 -epoches 8 -df data/test.csv -mi {i}\
+#     -id {j} -desc {j}\
+#       ')
