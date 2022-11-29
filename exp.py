7b65e8efd46150061c8de125b23ac3e0b22c8ceb
labadier <rlabadiet@gmail.com>
2022-11-29 20:42:37 +0100

    added cross data evaluation

--- exp.py
+++ exp.py
@@ -6,11 +6,23 @@ for i in range(3):
   for j in range(3):
     for k in range(j+1, 3):
 
+      l = [m for m in range(3) if m not in[j, k] ][0]
+      os.system(f"python main.py -model encoder -mode predict -wm online -l ml\
+         -bs 64 -df data/test.csv -output preds/{params.model_mult[i].split('/')[-1]} \
+          -mi {i} -wp logs/{params.model_mult[i].split('/')[-1]}/{params.model_mult[i].split('/')[-1]}_ml_{interest[j]}_{interest[k]}.pt\
+          -desc {interest[j]}_{interest[k]}--{interest[l]}")
 
-      os.system(f'\
-      python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l ml -bs 64 -epoches 8 -df data/test.csv -mi {i}\
-      -id {interest[j]}_{interest[k]} -desc {interest[j]}_{interest[k]}\
-        ')
+
+#interest = ['HaHackathon', 'joker', 'headlines']
+#for i in range(3):
+#  for j in range(3):
+#    for k in range(j+1, 3):
+
+
+#      os.system(f'\
+#      python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l ml -bs 64 -epoches 8 -df data/test.csv -mi {i}\
+#      -id {interest[j]}_{interest[k]} -desc {interest[j]}_{interest[k]}\
+#        ')
 
 # for i in range(3):
 #     os.system(f'python main.py -model encoder -mode train -lr 1e-5 -wm online -tf data/train.csv -l ml -bs 64 -epoches 8 -df data/test.csv -mi {i}')
